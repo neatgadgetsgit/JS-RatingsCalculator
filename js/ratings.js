62768840ff89a3b9e4b11649d24bc741bf4af80c
neatgadgetsgit <neatgadgets@gmail.com>
2022-01-21 15:56:28 +1000

    forEach

--- js/ratings.js
+++ js/ratings.js
@@ -2,5 +2,7 @@ const collect_ratings = () => {
     const ratings = {'count': 0, 'sum':0, 'average':0};
     let rating = 0;
     const elements = document.querySelectorAll('.rating');
-
+    elements.forEach((element) => {
+        
+    });
 }
