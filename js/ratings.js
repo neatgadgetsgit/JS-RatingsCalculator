28a76987be56b1f0f5034dd05d19bb7129ed7537
neatgadgetsgit <neatgadgets@gmail.com>
2022-01-26 10:23:47 +1000

    Update ratings.js
    sum and count to ratings not rating

--- js/ratings.js
+++ js/ratings.js
@@ -4,7 +4,7 @@ const collect_ratings = () => {
     const elements = document.querySelectorAll('.rating');
     elements.forEach((element) => {
         rating = parseInt(element.id.replace("star",""))
-        rating.count += parseInt(element.value)
-        rating.sum += parseInt(element.value) * rating
+        ratings.count += parseInt(element.value)
+        ratings.sum += parseInt(element.value) * rating
     });
 }
